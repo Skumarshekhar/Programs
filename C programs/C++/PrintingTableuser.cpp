a1295c7bf65fd8dcb17a1b484a7e3f22112a3fe9
Sandip Kumar Shekhar <sandipks1298@gmail.com>
2022-12-14 11:19:08 +0530

    14 December 2022

---
+++ C programs/C++/PrintingTableuser.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int number;
+    cout<<"Enter a number of which table you want to print\n";
+    cin>>number;
+    cout<<number<<" * 1 = "<<number*1;
+    return 0;
+
+}
---
+++ C programs/C++/python/Calendar.py
@@ -0,0 +1,4 @@
+import calendar
+yy = int(input("Enter Year"))
+mm = int(input("Enter month"))
+print(calendar.month(yy,mm))
