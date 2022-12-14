f135a4351500c1425cfaf53b393077dff6d7bdb2
Sandip Kumar Shekhar <sandipks1298@gmail.com>
2022-12-14 09:32:03 +0530

    14 December 2022

---
+++ C programs/C++/printingTable.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"4*1 = 4\n";
+    cout<<"4*2 = 8\n";
+    cout<<"4*3 = 12\n";
+    cout<<"4*4 = 16\n";
+    cout<<"4*5 = 20\n";
+    cout<<"4*6 = 24\n";
+    cout<<"4*7 = 28\n";
+    cout<<"4*8 = 32\n";
+    cout<<"4*9 = 36\n";
+    cout<<"4*10 = 40\n";
+    return 0;
+}
